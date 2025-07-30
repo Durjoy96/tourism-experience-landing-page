44d92966af175ca239dd5153c6732729a98b0058
Durjoy96 <mddurjoy853@gmail.com>
2025-07-30 13:05:08 +0600

    add parallax effect on the hero image

--- src/app/home/Hero.jsx
+++ src/app/home/Hero.jsx
@@ -1,15 +1,17 @@
+"use client";
+
 import React from "react";
 import heroImg from "@/assets/images/hero-img.png";
-import { ArrowRight } from "lucide-react";
 import PrimaryBtn from "@/components/primary-btn";
 import LinkBtn from "@/components/link-btn";
-
+import SimpleParallax from "simple-parallax-js";
+import Image from "next/image";
 export default function Hero() {
   return (
-    <div
-      className={`relative bg-cover bg-center bg-fixed min-h-[1024px]`}
-      style={{ backgroundImage: `url(${heroImg.src})` }}
-    >
+    <div className="relative">
+      <SimpleParallax scale={1.25} delay={1} transition="cubic-bezier(0,0,0,1)">
+        <Image src={heroImg} className="w-full min-h-screen" />
+      </SimpleParallax>
       <div className="absolute top-4/6 left-1/2 transform -translate-x-1/2 -translate-y-1/2 mt-12 text-center max-w-3xl  flex flex-col items-center">
         <h1 className="uppercase font-lora text-7xl font-semibold text-white/90">
           Unforgettable Experiences Await
