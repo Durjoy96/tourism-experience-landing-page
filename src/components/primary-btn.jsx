ea65cedd348c39f1bf3ab0156d01d05149b5c175
Durjoy96 <mddurjoy853@gmail.com>
2025-08-04 13:48:54 +0600

    add text-nowrap to the primary-btn component

--- src/components/primary-btn.jsx
+++ src/components/primary-btn.jsx
@@ -8,7 +8,7 @@ export default function PrimaryBtn({ text, onClick }) {
       <button
         onClick={onClick}
         type="button"
-        className="bg-primary text-primary-content font-medium text-base uppercase pl-10 pr-24 py-3 rounded-sm flex items-center justify-center gap-2 hover:bg-primary/90 transition-colors delay-150 duration-300 cursor-pointer group"
+        className="bg-primary text-primary-content font-medium text-sm uppercase pl-10 pr-24 py-3 rounded-sm flex items-center justify-center gap-2 hover:bg-primary/90 transition-colors delay-150 duration-300 cursor-pointer group text-nowrap"
         style={{
           clipPath: "polygon(0% 0%, 75% 0%, 82% 50%, 75% 100%, 0% 100%)",
         }}
