0c17bdfec213187e2bdfe13d1494064f23f346a4
Durjoy96 <mddurjoy853@gmail.com>
2025-08-08 12:22:06 +0600

    add section gap for mobile screens

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,22 +8,22 @@ import Stories from "./home/Stories";
 export default function Home() {
   return (
     <>
-      <section className="pb-12 bg-base-200">
+      <section className="pb-8 md:pb-12 bg-base-200">
         <Hero />
       </section>
-      <section className="pt-12 pb-32 bg-base-200">
+      <section className="pt-8 md:pt-12 pb-28 md:pb-32 bg-base-200">
         <Stories />
       </section>
-      <section className="py-12 bg-base-200">
+      <section className="py-8 md:py-12 bg-base-200">
         <Journey />
       </section>
-      <section className="py-12 bg-base-200">
+      <section className="py-8 md:py-12 bg-base-200">
         <Guide />
       </section>
-      <section className="py-12 bg-base-200">
+      <section className="py-8 md:py-12 bg-base-200">
         <MostBookedPlaces />
       </section>
-      <section className="py-12 bg-base-200">
+      <section className="py-8 md:py-12 bg-base-200">
         <JoinCommunity />
       </section>
     </>
