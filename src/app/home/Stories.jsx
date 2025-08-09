38b7fa5169b492780b2f56879dad7b41208d331a
Durjoy96 <mddurjoy853@gmail.com>
2025-08-09 23:33:46 +0600

    Stories component animation done

--- src/app/home/Stories.jsx
+++ src/app/home/Stories.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import React from "react";
 import happyMemories from "@/assets/images/happy-memories.png";
@@ -5,6 +7,7 @@ import CustomersFeedback from "@/components/customers-feedback";
 import WomenCustomer from "@/assets/images/women-customer.jpg";
 import MenCustomer from "@/assets/images/men-customer.jpg";
 import PrimaryBtn from "@/components/primary-btn";
+import { motion } from "motion/react";
 
 export default function Stories() {
   return (
@@ -16,7 +19,13 @@ export default function Stories() {
         >
           we make journeys unforgettable
         </h2>
-        <div className="grid gap-6 md:gap-8 mt-8 md:mt-16">
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 0.8 }}
+          viewport={{ once: true }}
+          className="grid gap-6 md:gap-8 mt-8 md:mt-16"
+        >
           <CustomersFeedback
             img={WomenCustomer}
             name="amara t."
@@ -27,27 +36,39 @@ export default function Stories() {
             name="Jesse M."
             description="We don't just tour tokyo, we lived it for a day."
           />
-        </div>
+        </motion.div>
         <div className="mt-8 md:mt-12">
           <PrimaryBtn text="more stories" />
         </div>
       </div>
       <div className="flex justify-end md:justify-center lg:justify-end">
-        <div className="max-w-[300px] md:max-w-md h-[350px] md:h-[448px] relative">
+        <motion.div
+          initial={{ opacity: 0, y: 150 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8 }}
+          viewport={{ once: true }}
+          className="max-w-[300px] md:max-w-md h-[350px] md:h-[448px] relative"
+        >
           <Image
             src={happyMemories}
             className="w-full h-full object-cover object-center"
             alt="Happy Memories"
           />
-          <div className="w-[150px] h-[150px] md:w-[200px] md:h-[200px] bg-primary-content absolute -bottom-[80px] -left-[20px] px-4 md:px-6 py-6 md:py-8 flex flex-col justify-between">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            whileInView={{ opacity: 1, scale: [0, 1.2, 1] }}
+            transition={{ duration: 0.5, delay: 0.3 }}
+            viewport={{ once: true }}
+            className="w-[150px] h-[150px] md:w-[200px] md:h-[200px] bg-primary-content absolute -bottom-[80px] -left-[20px] px-4 md:px-6 py-6 md:py-8 flex flex-col justify-between"
+          >
             <span className="text-white font-semibold text-2xl md:text-5xl">
               500k
             </span>
             <span className="text-white font-normal text-xs md:text-sm uppercase">
               more than 500,000 memories made
             </span>
-          </div>
-        </div>
+          </motion.div>
+        </motion.div>
       </div>
     </div>
   );
